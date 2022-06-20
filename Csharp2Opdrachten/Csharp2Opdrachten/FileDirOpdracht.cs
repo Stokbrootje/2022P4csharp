921882f03a2bc591b87096470410a12b959c8f3c
Stokbrootje <>
2022-06-20 14:27:59 +0200

    oui

---
+++ Csharp2Opdrachten/Csharp2Opdrachten/FileDirOpdracht.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace Csharp2Opdrachten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\leonk");
+            foreach (var sub in dir.GetFiles())
+            {
+                Console.Write(sub.Name);
+            }
+        }
+    }
+}
---
+++ Csharp2Opdrachten/Csharp2Opdrachten/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Csharp2Opdrachten
+{
+    internal class Program
+    {
+        public void Hello()
+        {
+            Console.WriteLine("Hello");
+        }
+    } 
+
+}
